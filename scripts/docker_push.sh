8c9491d745f5289613b66b319edd481378f84fd5
Brant Burnett <bburnett@centeredgesoftware.com>
2021-07-13 14:53:06 -0400

    Switch to GitHub Actions (#10)

--- scripts/docker_push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD";
-docker tag centeredge/shawarma:${TRAVIS_BUILD_NUMBER} centeredge/shawarma:${TRAVIS_TAG};
-docker tag centeredge/shawarma:${TRAVIS_BUILD_NUMBER} centeredge/shawarma:latest;
-docker push centeredge/shawarma:${TRAVIS_TAG};
-docker push centeredge/shawarma:latest;

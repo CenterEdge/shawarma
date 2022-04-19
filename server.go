86f35812502ff8b57559f60743643c7ad319c30c
Ricardo Temperini <ricardo.temperini@glovoapp.com>
2022-04-19 12:56:35 +0200

    remove explicit env port to use cli config

--- server.go
+++ server.go
@@ -4,19 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"os"
 
 	log "github.com/sirupsen/logrus"
 )
-
-// get OS environment variable, fallback as default
-func getenv(key, fallback string) string {
-	value := os.Getenv(key)
-	if len(value) == 0 {
-		return fallback
-	}
-	return value
-}
 
 // Handlers
 func deploymentStatus(w http.ResponseWriter, req *http.Request) {
@@ -39,14 +29,12 @@ func _health(w http.ResponseWriter, req *http.Request) {
 }
 
 // Http Server
-func httpServer(default_port string) {
+func httpServer(port string) {
 
 	// Endpoints Handlers
 	http.HandleFunc("/deploymentstatus", deploymentStatus)
 	http.HandleFunc("/_health", _health)
 
-	// Get OS envs
-	port := getenv("LISTEN_PORT", default_port)
 	log.Info(fmt.Sprintf("Starting HTTP Server on port %s", port))
 
 	// Listener
